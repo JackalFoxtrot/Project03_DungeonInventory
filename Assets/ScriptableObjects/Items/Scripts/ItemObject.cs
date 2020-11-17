a5fd3cd105eb1c603025e9457267fa5055de6912
Jacob Rose <jhr160230@utdallas.edu>
2020-11-16 19:11:43 -0600

    Inventory v1
    Initial inventory set up with Scriptable objects going into a dictionary inventory system.

---
+++ Assets/ScriptableObjects/Items/Scripts/ItemObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Consumable,
+    Equipment,
+    Key,
+    Default
+}
+public abstract class ItemObject : ScriptableObject
+{
+    public GameObject prefab;
+    public ItemType type;
+    [TextArea(5, 10)]
+    public string itemname;
+    [TextArea(5, 10)]
+    public string specificType;
+    [TextArea(15,20)]
+    public string description;
+
+    public bool identified;
+    public bool favorited;
+
+    public int value;
+    public int rarity;
+}
